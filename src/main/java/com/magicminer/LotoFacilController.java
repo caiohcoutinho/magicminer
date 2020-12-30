3f9b3cc207e0afe44ea3febb5d1d0b20670076d2
Caio Coutinho <caiohcoutinho@gmail.com>
2020-12-30 14:47:03 -0300

    Algorihtm running on all valid games
    Change-Id: I3c5580ce1f67014836f0ac53d47af6d7ca8c647b

--- src/main/java/com/magicminer/LotoFacilController.java
+++ src/main/java/com/magicminer/LotoFacilController.java
@@ -1,10 +1,13 @@
 package com.magicminer;
 
+import com.magicminer.ai.Algorithm;
 import com.magicminer.ai.constant.ConstantAlgorithm;
 import com.magicminer.ai.genetic.GeneticAlgorithm;
 import com.magicminer.ai.random.RandomAlgorithm;
 import com.magicminer.model.LotoFacilGame;
+import com.magicminer.model.entity.Game;
 import com.magicminer.model.entity.GameCustomRepository;
+import com.magicminer.model.entity.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -24,50 +27,41 @@ public class LotoFacilController {
     @Autowired
     private GameCustomRepository gameCustomRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @RequestMapping(value = "/lotofacil/{gameTableId}", method = RequestMethod.GET)
     public ResponseEntity<List<LotoFacilGame>> getGameList(@PathVariable("gameTableId") Long gameTableId) {
         return ResponseEntity.ok(gameCustomRepository.getGamesByGameTableId(gameTableId));
     }
 
     @RequestMapping(value = "/lotofacil/random", method = RequestMethod.GET)
-    public ResponseEntity<List<LotoFacilGame>> runRandomAlgorithm(@RequestParam(name="seed", required = false) Long seed) {
-        List<LotoFacilGame> games = new ArrayList<>(LotoFacilGame.CLASSIC_RESULTS.values());
+    public ResponseEntity<List<LotoFacilGame>> runRandomAlgorithm(@RequestParam(name="seed", required = false) Long seed,
+                                                                  @RequestParam(name="gameTableId", required = false) Long gameTableId) {
         RandomAlgorithm algorithm = seed != null ? RandomAlgorithm.createBySeed(seed) : RandomAlgorithm.create();
-        List<LotoFacilGame> result = games.stream()
-                .filter(g -> g.getValid())
-                .sorted(Comparator.comparingInt(LotoFacilGame::getGameNumber))
-                .map(g -> {
-                    LotoFacilGame execution = algorithm.run();
-                    execution.score(g);
-                    execution.setGameNumber(g.getGameNumber());
-                    return execution;
-                }).collect(Collectors.toList());
+        List<LotoFacilGame> result = runAlgorithm(gameTableId, algorithm);
         return ResponseEntity.ok(result);
     }
 
     @RequestMapping(value = "/lotofacil/genetic", method = RequestMethod.GET)
-    public ResponseEntity<List<LotoFacilGame>> runGeneticAlgorithm(@RequestParam(name="seed", required = false) Long seed) {
-        List<LotoFacilGame> games = new ArrayList<>(LotoFacilGame.CLASSIC_RESULTS.values());
+    public ResponseEntity<List<LotoFacilGame>> runGeneticAlgorithm(@RequestParam(name="seed", required = false) Long seed,
+                                                                   @RequestParam(name="gameTableId", required = false) Long gameTableId) {
         GeneticAlgorithm algorithm = seed != null ? GeneticAlgorithm.createBySeed(seed) : GeneticAlgorithm.create();
-        algorithm.setGames(games);
-        algorithm.generateSuperSpecimen();
-        List<LotoFacilGame> result = games.stream()
-                .filter(g -> g.getValid())
-                .sorted(Comparator.comparingInt(LotoFacilGame::getGameNumber))
-                .map(g -> {
-                    LotoFacilGame execution = algorithm.run();
-                    execution.score(g);
-                    execution.setGameNumber(g.getGameNumber());
-                    return execution;
-                }).collect(Collectors.toList());
+        List<LotoFacilGame> result = runAlgorithm(gameTableId, algorithm);
         return ResponseEntity.ok(result);
     }
 
     @RequestMapping(value = "/lotofacil/constant", method = RequestMethod.GET)
-    public ResponseEntity<List<LotoFacilGame>> runConstantAlgorithm(@RequestParam(name="seed", required = false) Long seed) {
-        List<LotoFacilGame> games = new ArrayList<>(LotoFacilGame.CLASSIC_RESULTS.values());
+    public ResponseEntity<List<LotoFacilGame>> runConstantAlgorithm(@RequestParam(name="seed", required = false) Long seed,
+                                                                    @RequestParam(name="gameTableId", required = false) Long gameTableId) {
         ConstantAlgorithm algorithm = seed != null ? ConstantAlgorithm.createBySeed(seed) : ConstantAlgorithm.create();
-        List<LotoFacilGame> result = games.stream()
+        List<LotoFacilGame> result = runAlgorithm(gameTableId, algorithm);
+        return ResponseEntity.ok(result);
+    }
+
+    private List<LotoFacilGame> runAlgorithm(Long gameTableId, Algorithm<LotoFacilGame> algorithm) {
+        List<LotoFacilGame> games = gameCustomRepository.getGamesByGameTableId(gameTableId);
+        return games.stream()
                 .filter(g -> g.getValid())
                 .sorted(Comparator.comparingInt(LotoFacilGame::getGameNumber))
                 .map(g -> {
@@ -76,7 +70,5 @@ public class LotoFacilController {
                     execution.setGameNumber(g.getGameNumber());
                     return execution;
                 }).collect(Collectors.toList());
-
-        return ResponseEntity.ok(result);
     }
 }
